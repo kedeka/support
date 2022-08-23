abb9b13579a4f0e7a46526f3e400303c4bcfcb95
riskihajar <riskihajar@gmail.com>
2022-08-23 09:25:03 +0800

    register paginator

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,10 +3,16 @@
 namespace Kedeka\Support;
 
 use Exception;
+
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Collection;
 use Illuminate\Support\HtmlString;
+use Illuminate\Pagination\UrlWindow;
+use Illuminate\Pagination\LengthAwarePaginator;
+
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -30,6 +36,11 @@ class ServiceProvider extends PackageServiceProvider
     {
         $this->bootBlueprint();
         $this->bootBlade();
+    }
+
+    public function packageRegistered()
+    {
+        $this->registerLengthAwarePaginator();
     }
 
     protected function bootMacro()
@@ -79,6 +90,89 @@ class ServiceProvider extends PackageServiceProvider
             }
 
             throw new Exception('CSS File Not found');
+        });
+    }
+
+    protected function registerLengthAwarePaginator()
+    {
+        $this->app->bind(LengthAwarePaginator::class, function ($app, $values) {
+            return new class(...array_values($values)) extends LengthAwarePaginator
+            {
+                public function only(...$attributes)
+                {
+                    return $this->transform(function ($item) use ($attributes) {
+                        return $item->only($attributes);
+                    });
+                }
+
+                public function transform($callback)
+                {
+                    $this->items->transform($callback);
+
+                    return $this;
+                }
+
+                public function merge($attributes = [])
+                {
+                    $this->merge = $attributes;
+                    return $this;
+                }
+
+                public function toArray()
+                {
+                    return [
+                        'data' => $this->items->toArray(),
+                        'links' => $this->links(),
+                        'from' => (($this->currentPage() - 1) * $this->perPage()) + 1,
+                        'prev' => [
+                            'url' => $this->previousPageUrl(),
+                            'label' => 'Previous',
+                            'active' => false
+                        ],
+                        'next' => [
+                            'url' => $this->nextPageUrl(),
+                            'label' => 'Next',
+                            'active' => false,
+                        ],
+                        ...$this->merge ?? []
+                    ];
+                }
+
+                public function links($view = null, $data = [])
+                {
+                    $this->appends(Request::all());
+
+                    $window = UrlWindow::make($this);
+
+                    $elements = array_filter([
+                        $window['first'],
+                        is_array($window['slider']) ? '...' : null,
+                        $window['slider'],
+                        is_array($window['last']) ? '...' : null,
+                        $window['last'],
+                    ]);
+
+                    return Collection::make($elements)->flatMap(function ($item) {
+                        if (is_array($item)) {
+                            return Collection::make($item)->map(function ($url, $page) {
+                                return [
+                                    'url' => $url,
+                                    'label' => $page,
+                                    'active' => $this->currentPage() === $page,
+                                ];
+                            });
+                        } else {
+                            return [
+                                [
+                                    'url' => null,
+                                    'label' => '...',
+                                    'active' => false,
+                                ],
+                            ];
+                        }
+                    });
+                }
+            };
         });
     }
 }
